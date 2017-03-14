b808ba434f0bb67b12c1784eefe900ce6ad3c99d
rakin <rakin@localhost.localdomain>
2017-03-14 11:36:43 +0600

    url to action

--- src/Route.elm
+++ src/Route.elm
@@ -1,10 +1,15 @@
 module Route exposing (..)
 
 import Model exposing (Model)
-import Message exposing (Msg) 
+import Message exposing (Msg)
+import Menu.Message as MenuMessage
+import Menu.Model as MenuModel
 import RouteUrl exposing (UrlChange)
 import RouteUrl.Builder as Builder exposing (Builder, builder)
 import Navigation exposing (Location)
+import Menu.Data as MenuData
+import Debug
+
 
 delta2url : Model -> Model -> Maybe UrlChange
 delta2url pre cur =
@@ -12,11 +17,12 @@ delta2url pre cur =
         delta2builder pre cur
 
 
-delta2builder : Model -> Model -> Maybe Builder  
+delta2builder : Model -> Model -> Maybe Builder
 delta2builder pre cur =
-    Just
-        <| Builder.replacePath [cur.menu.current]
-        <| Builder.builder
+    Just <|
+        Builder.replacePath [ cur.menu.current ] <|
+            Builder.builder
+
 
 url2messages : Location -> List Msg
 url2messages location =
@@ -25,4 +31,57 @@ url2messages location =
 
 builder2messages : Builder -> List Msg
 builder2messages builder =
-    []
+    case Builder.path builder of
+        first :: rest ->
+            case (Debug.log "first" first) of
+                "Role" ->
+                    [ Message.Menu MenuMessage.Role ]
+
+                "Logout" ->
+                    [ Message.Menu MenuMessage.Logout ]
+
+                "User" ->
+                    [ Message.Menu MenuMessage.User ]
+
+                "Me" ->
+                    [ Message.Menu MenuMessage.Me ]
+
+                "Sell" ->
+                    [ Message.Menu <|
+                        MenuMessage.SelectTab <|
+                            tabInd "Sell"
+                    ]
+
+                "Product" ->
+                    [ Message.Menu <|
+                        MenuMessage.SelectTab <|
+                            tabInd "Product"
+                    ]
+
+                "Order" ->
+                    [ Message.Menu <|
+                        MenuMessage.SelectTab <|
+                            tabInd "Order"
+                    ]
+
+                "Report" ->
+                    [ Message.Menu <|
+                        MenuMessage.SelectTab <|
+                            tabInd "Report"
+                    ]
+
+                _ ->
+                    []
+
+        _ ->
+            []
+
+
+tabInd : String -> Int
+tabInd val =
+    case MenuData.tabInd val of
+        Just ind ->
+            ind
+
+        _ ->
+            -1
